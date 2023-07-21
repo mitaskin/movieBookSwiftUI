d1a7521a33fd0e9b6c5e9e7e96b4cd39f46d3e09
mitaskin <mi.taskin027@gmail.com>
2023-07-21 13:13:24 +0300

    Initial Commit

---
+++ movieBookSwiftUI/movieBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  movieBookSwiftUIApp.swift
+//  movieBookSwiftUI
+//
+//  Created by Muhammed ikbal TAŞKIN on 21.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct movieBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
