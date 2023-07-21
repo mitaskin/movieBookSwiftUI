7b1181e446b8d1e0ccd8cd0cfbdb6088e5f6ee0a
mitaskin <mi.taskin027@gmail.com>
2023-07-21 16:10:45 +0300

    created downloaderClient code

--- movieBookSwiftUI/Service/DownloaderClient.swift
+++ movieBookSwiftUI/Service/DownloaderClient.swift
@@ -9,4 +9,37 @@ import Foundation
 
 class DownloaderClient {
     
+    func filmleriIndir(search : String, completion: @escaping (Result<[Film]?,DowloaderError>) -> Void)
+    {
+        
+        
+        guard let url = URL(string: "https://www.omdbapi.com/?s=\(search)&apikey=befa1d0e") else
+        {
+            return completion(.failure(.yanlisUrl))
+        }
+        
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            guard let data = data, error == nil else
+            {
+                return completion(.failure(.veriGelmedi))
+            }
+            
+            guard let filmCevabi = try? JSONDecoder().decode(GelenFilmler.self, from: data) else
+            {
+                return completion(.failure(.veriIslenemedi))
+            }
+            
+            completion(.success(filmCevabi.filmler))
+            
+        }.resume()
+    }
+    
+}
+
+enum DowloaderError : Error{
+    case yanlisUrl
+    case veriGelmedi
+    case veriIslenemedi
 }
